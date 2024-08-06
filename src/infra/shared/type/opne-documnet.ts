2db1db18f392558f3b717916e3f1bb98b7e56eca
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2024-08-06 18:02:56 +0500

    some changes

--- src/infra/shared/type/opne-documnet.ts
+++ src/infra/shared/type/opne-documnet.ts
@@ -1,2 +1,2 @@
-type OpenDocumentType = "budget legislation information" | "PF - 6742" | "organizations included"
+type OpenDocumentType = "budget legislation information" | "PF - 6742" | "organizations included" | "subisiyda information"
 export default OpenDocumentType;
